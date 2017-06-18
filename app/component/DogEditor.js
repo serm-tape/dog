16e76a9fdd7c666b42c48c90ccc3bb0d2c629499
serm-tape <serm.thm@gmail.com>
2017-06-18 20:02:46 +0700

    Add location detection

--- app/component/DogEditor.js
+++ app/component/DogEditor.js
@@ -2,6 +2,24 @@ import React from 'react'
 import {LabeledInput} from './LabeledInput'
 
 class DogEditor extends React.Component{
+    componentDidMount(){
+        const mark = [{lat: -25.363, lng: 131.044}]
+        const map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 3,
+            center: mark[0]
+        })
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+                (pos) => {
+                    new google.maps.Marker({
+                        position: {lat: pos.coords.latitude, lng: pos.coords.longitude},
+                        map: map,
+                        title: 'My location'
+                    })
+                }
+            )
+        }
+    }
     render(){
         return (
             <div id='dog-editor' className='ui modal'>
@@ -13,6 +31,7 @@ class DogEditor extends React.Component{
                     <LabeledInput label='text' type='text' placeholder='some text ...' /><br />
                     <LabeledInput label='number' type='numner' placeholder='555' /><br />
                     <LabeledInput label='email' type='email' placeholder='user@mail.com' /><br />
+                    <div id='map' style={{height:300}}></div>
                 </div>
                 <div className='actions'>
                     <div className='ui positive button'>
